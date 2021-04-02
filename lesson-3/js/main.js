fa791bc66104aa8625da256b1b8410fe0f6e4725
Oleh-99 <cyaputa99@gmail.com>
2021-04-02 14:11:18 +0300

    add edit

--- lesson-3/js/main.js
+++ lesson-3/js/main.js
@@ -1,169 +1,130 @@
-// TAGS
+// TABS
 
-var $buttonTags = document.getElementsByClassName('btnTags');
+var $buttonTags = document.getElementsByClassName('buttonTag');
+var $tagContent = document.getElementsByClassName('tagContent');
 
 for (let i = 0; i < $buttonTags.length; i++) {
 	var $btnTags = $buttonTags[i];
 	$btnTags.addEventListener('click', function () { 
-		var $new;
-		var $bestsellers;
-		var $commingSoon;
 
 		for (let d = 0; d < $buttonTags.length; d++) {
 			if($buttonTags[d].classList.contains('active')) {
 				$buttonTags[d].classList.remove('active');
-				$buttonTags[d].classList.add('transparentNew');
 			}
 		}
 
-		if (i === 0) {
-			$new = 'display: block';
-			$bestsellers = 'display: none';
-			$commingSoon = 'display: none';
-		}
+		for (let k = 0; k < $tagContent.length; k++) {
+			var tagInner = $tagContent[k];
+			tagInner.classList.remove('active');
 
-		if (i === 1) {
-			$new = 'display: none';
-			$bestsellers = 'display: block';
-			$commingSoon = 'display: none';
-		}
+			if (tagInner.dataset.value === this.dataset.btn) {
+				tagInner.classList.add('active');
+			}
+		}		
 
-		if (i === 2) {
-			$new = 'display: none';
-			$bestsellers = 'display: none';
-			$commingSoon = 'display: block';
-		}
-
-		document.getElementsByClassName('new')[0].style = $new;
-		document.getElementsByClassName('bestsellers')[0].style = $bestsellers;
-		document.getElementsByClassName('commingSoon')[0].style = $commingSoon;
 		this.classList.add('active');
-		this.classList.remove('transparentNew');
 	});
 }
 
 // ACARDION
 
-var $buttonAcard = document.getElementsByClassName('btn-acard');
+var $buttonAcard = document.getElementsByClassName('question-inner');
+var $textAcard = document.getElementsByClassName('question-text');
 
 for (let i = 0; i < $buttonAcard.length; i++) {
 	var $btnAcard = $buttonAcard[i];
 	$btnAcard.addEventListener('click', function () { 
-		var $textAcar1;
-		var $textAcar2;
-		var $textAcar3;
-		var $textAcar4;
 
 		for (let d = 0; d <  $buttonAcard.length; d++) {
 			if ($buttonAcard[d].classList.contains('active')) {
 				$buttonAcard[d].classList.remove('active');
-				document.getElementsByClassName('btn-acard')[d].style.transform = 'rotate(0)';
 			}
 		}
 
-		if (i === 0) {
-			$textAcar1 = 'display: block';
-			$textAcar2 = 'display: none';
-			$textAcar3 = 'display: none';
-			$textAcar4 = 'display: none';
-		}
+		for (let k = 0; k <	$textAcard.length; k++) {
+			var textInner = $textAcard[k];
 
-		if (i === 1) {
-			$textAcar1 = 'display: none';
-			$textAcar2 = 'display: block';
-			$textAcar3 = 'display: none';
-			$textAcar4 = 'display: none';
-		}
+			if ($textAcard[k].classList.contains('active')) {
+				textInner.classList.remove('active');
+				this.classList.remove('active');
 
-		if (i === 2) {
-			$textAcar1 = 'display: none';
-			$textAcar2 = 'display: none';
-			$textAcar3 = 'display: block';
-			$textAcar4 = 'display: none';
-		}
-
-		if (i === 3) {
-			$textAcar1 = 'display: none';
-			$textAcar2 = 'display: none';
-			$textAcar3 = 'display: none';
-			$textAcar4 = 'display: block';
-		}
-
-		document.getElementsByClassName('question-text1')[0].style = $textAcar1;
-		document.getElementsByClassName('question-text2')[0].style = $textAcar2;
-		document.getElementsByClassName('question-text3')[0].style = $textAcar3;
-		document.getElementsByClassName('question-text4')[0].style = $textAcar4;
-		document.getElementsByClassName('btn-acard')[i].style.transform = 'rotate(180deg)';
-		this.classList.add('active');
+			} else {
+				
+				if (textInner.dataset.value === this.dataset.btn) {
+					textInner.classList.add('active');
+					$buttonAcard[i].classList.add('active');
+				}
+			}
+		}	
 	});
 }
 
+
 // ACARDION V. 2
 
-var $buttonAcardV2 = document.getElementsByClassName('btn-acardion');
+
+var $buttonAcardV2 = document.getElementsByClassName('question-innerV2');
+var $textAcardV2 = document.getElementsByClassName('question-textV2');
 
 for (let i = 0; i < $buttonAcardV2.length; i++) {
 	var $btnAcardV2 = $buttonAcardV2[i];
 	$btnAcardV2.addEventListener('click', function () { 
-		var $textAcar1;
-		var $textAcar2;
-		var $textAcar3;
-		var $textAcar4;
 
-		if (i === 0) {
-			$textAcar1 = 'display: block';
-			document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(180deg)';
-			console.log(i);
+		for (let k = 0; k <	$textAcardV2.length; k++) {
+			var textInnerV2 = $textAcardV2[k];
 
-			if (this.classList.contains('active')) {
-				this.classList.remove('active');
-				document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(0)';
-				$textAcar1 = 'display: none';
+			if (textInnerV2.dataset.value === this.dataset.btn) {
+
+				if (textInnerV2.classList.contains('active')) {
+					textInnerV2.classList.remove('active');
+					$buttonAcardV2[i].classList.remove('active');
+				} else {
+					textInnerV2.classList.add('active');
+					$buttonAcardV2[i].classList.add('active');
+				}
+				
+			}							
+		}
+	});
+}
+
+// Slider 
+
+var $buttonSlider = document.getElementsByClassName('arrow-inner');
+var $foto = document.getElementsByClassName('fotoNew');
+
+for (let i = 0; i < $buttonSlider.length; i++) {
+	var $btnSlider = $buttonSlider[i];
+	$btnSlider.addEventListener('click', function () { 
+		var fotoNumb;
+
+		for (let d = 0; d <  $foto.length; d++) {
+			if ($foto[d].classList.contains('active')) {
+				fotoNumb = d + 1;
+				$foto[d].classList.remove('active');
 			}
-			this.classList.add('active');
 		}
 
-		if (i === 1) {
-			$textAcar2 = 'display: block';
-			document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(180deg)';
-
-			if ($btnAcardV2.classList.contains('active')) {
-				$btnAcardV2.classList.remove('active');
-				document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(0)';
-				$textAcar2 = 'display: none';
-			}
-			this.classList.add('active');
+		if (this.dataset.direction === '1') {
+			fotoNumb ++;
 		}
 
-		if (i === 2) {
-			$textAcar3 = 'display: block';
-			document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(180deg)';
-
-			if ($btnAcardV2.classList.contains('active')) {
-				$btnAcardV2.classList.remove('active');
-				document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(0)';
-				$textAcar3 = 'display: none';
-			}
-			this.classList.add('active');
+		if (this.dataset.direction === '-1') {
+			fotoNumb --;
 		}
 
-		if (i === 3) {
-			$textAcar4 = 'display: block';
-			document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(180deg)';
-
-			if ($btnAcardV2.classList.contains('active')) {
-				$btnAcardV2.classList.remove('active');
-				document.getElementsByClassName('btn-acardion')[i].style.transform = 'rotate(0)';
-				$textAcar4 = 'display: none';
-			}
-			this.classList.add('active');
+		if ($foto.length + 1 <= fotoNumb) {
+			fotoNumb = 1;
 		}
 
-		document.getElementsByClassName('question-acardion-text1')[0].style = $textAcar1;
-		document.getElementsByClassName('question-acardion-text2')[0].style = $textAcar2;
-		document.getElementsByClassName('question-acardion-text3')[0].style = $textAcar3;
-		document.getElementsByClassName('question-acardion-text4')[0].style = $textAcar4;
-		
-		
+		if (0 === fotoNumb) {
+			fotoNumb = $foto.length;
+		}
+
+		for (let d = 0; d <  $foto.length; d++) {
+			if (d + 1 === fotoNumb) {
+				$foto[d].classList.add('active');
+			}
+		}
 	});
 }

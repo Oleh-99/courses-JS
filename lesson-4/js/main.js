3e1dee0cb2a16e7f2038363a5042713e5861b228
Oleh-99 <cyaputa99@gmail.com>
2021-04-08 12:52:04 +0300

    add edit

--- lesson-4/js/main.js
+++ lesson-4/js/main.js
@@ -1,20 +1,16 @@
 (function($) {
-$(document).ready(function() {
 
-	var arrData = [ ];
-	
-	
-	var arrAlbum = [ ];
-	var arrFoto = [ ];
-	var arrTodo = [ ];
-
-
-	function addUsers () {
+	function addUsers() {
+		var arrData = [];
 		if (0 === arrData.length) {
+			loaderShow();
 			$.ajax({
 				url: 'https://jsonplaceholder.typicode.com/users', 
 				async: false,
-				success: function (data) { 
+				// beforeSend: function() {
+					
+				// },
+				success: function(data) { 
 				
 					for (let index = 0; index < data.length; index++) {
 						arrData.push(data[index]);
@@ -44,280 +40,294 @@ $(document).ready(function() {
 								<a href="#" class="btn btn-todos">Todos</a>
 							</div>`
 						);
-
-					}
-
-					// $.ajax({
-					// 	url: `https://jsonplaceholder.typicode.com/posts?userId=${arrData[0].id}`, 
-					// 	dataType : "json",
-					// 	success: function (data) { 
-					// 		for (let index = 0; index < data.length; index++) {
-					// 			arrPost.push(data[index]);
-					// 			$('.posts .row').append(`
-					// 				<div class="col-4 post" data-post="${data[index].id}">
-					// 					<div class="title">${data[index].title}</div>
-					// 					<div class="text-comm">${data[index].body} ...</div>
-					// 				</div>`
-					// 			)
-					// 		}
-					// 	}
-					// })
+					};
+					$('.user').first().find('.btn-posts').trigger('click');
+				},
+				error: function() {
+					alert('Error');
+				},
+				complete: function() {
+					loaderHide();
 				}
 			});
 		};
 	}
 
-
-	function addPostUser() {
+	function addUsersPost() {
 		var arrPost = [];
 		$(document).on('click', '.btn-posts', function(e) {
 			e.preventDefault();
 			
-
-			let $this = $(this)
+			let $this = $(this);
 			let $user = $this.parent();
 			let id = $user.data('id');
 
-			$('.content').append('<div class="loader"></div>');
-			$('.btn').removeClass('active');
-			$('.user').removeClass('active');
+			// $('.content').append('<div class="loader"></div>');
+			removeContent();
 			$this.addClass('active');
 			$user.addClass('active');
-			$('.post').remove();
-			$('.album').remove();
-			$('.album-foto').remove();
-			$('.todo').remove();
-			
 		
 			if (typeof arrPost['userId_' + id] === 'undefined') {
+				loaderShow();
 				$.ajax({
 					url: `https://jsonplaceholder.typicode.com/posts?userId=${id}`,
 					dataType : "json",
 					async: false,
+					// beforeSend: function() {
+						
+					// },
 					success: function (data) { 
 						arrPost['userId_' + id] = data;
+					},
+					error: function() {
+						alert('Error');
+					},
+					complete: function() {
+						loaderHide();
 					}
 				})
 			} 
 
 			for (let index = 0; index < arrPost[`userId_${id}`].length; index++) {
-				if (arrPost[`userId_${id}`][index].userId === id) {
-					$('.posts .row').append(`
-						<div class="col-4 post" data-post="${arrPost[`userId_${id}`][index].id}">
-							<div class="title">${arrPost[`userId_${id}`][index].title}</div>
-							<div class="text-comm">${arrPost[`userId_${id}`][index].body} ...</div>
-						</div>
-					`)
-				}
-			}
+				$('.posts .row').append(`
+					<div class="col-4 post" data-post="${arrPost[`userId_${id}`][index].id}">
+						<div class="title">${arrPost[`userId_${id}`][index].title}</div>
+						<div class="text-comm">${arrPost[`userId_${id}`][index].body} ...</div>
+						<button class="btn btn-comm">View comments</button>
+					</div>`
+				);
+			};
 			
-			setTimeout(function () {
-				$('.loader').remove()
-			}, 1000);
+			// setTimeout(function () {
+			// 	// $('.loader').remove();
+			// }, 1000);
 		});
 	}
 
-	function addComment () {
+	function addComment() {
 		var arrComm = [];
-		$(document).on('click', '.post', function() {
+		$(document).on('click', '.btn-comm', function() {
 			let $this = $(this);
-			let id = $this.data('post');
-			let availability = false;
+			let $thisParent = $this.parent();
+			let id = $thisParent.data('post');
 
-			$('.content').append('<div class="loader"></div>');
-			if($this.hasClass('active')) {
-				$this.removeClass('active');
-				$this.find('.comm').remove()
+			// $('.content').append('<div class="loader"></div>');
+			if ($this.hasClass('active')) {
+				$this.removeClass('active').text('View comments');
+				$thisParent.removeClass('active').find('.comm').remove();
 			} else {
-				$('.post').removeClass('active');
-				$this.addClass('active');
+				$this.addClass('active').text('Hidden comments');
+				$thisParent.addClass('active');
 
 				if (typeof arrComm['postId_' + id] === 'undefined') {
+					loaderShow();
 					$.ajax({
 						url: `https://jsonplaceholder.typicode.com/comments?postId=${id}`, 
 						dataType : "json",
 						async: false,
+						// beforeSend: function() {
+						// 	loaderShow();
+						// },
 						success: function (data) { 
 							arrComm['postId_' + id] = data;
+						},
+						error: function() {
+							alert('Error');
+						},
+						complete: function() {
+							loaderHide();
 						}
 					})
 				} 
 	
-				for (let index = 0; index < arrPost[`postId_${id}`].length; index++) {
-					if (arrComm[`postId_${id}`][index].userId === id) {
-						$this.append(
-							`<div class="comm" data-comm="${arrComm[`postId_${id}`][index].id}">
-								<div class="comm-name">${arrComm[`postId_${id}`][index].name}</div>
-								<div class="comm-email">${arrComm[`postId_${id}`][index].email}</div>
-								<div class="comm-body">${arrComm[`postId_${id}`][index].body}</div>
-							</div>`
-						);
-					}
+				for (let index = 0; index < arrComm[`postId_${id}`].length; index++) {
+					$thisParent.append(
+						`<div class="comm" data-comm="${arrComm[`postId_${id}`][index].id}">
+							<div class="comm-name">${arrComm[`postId_${id}`][index].name}</div>
+							<div class="comm-email">${arrComm[`postId_${id}`][index].email}</div>
+							<div class="comm-body">${arrComm[`postId_${id}`][index].body}</div>
+						</div>`
+					);
 				}
-			}
-			setTimeout(function () {
-				$('.loader').remove()
-			}, 1000);
+			};
+
+			// setTimeout(function () {
+			// 	// $('.loader').remove()
+			// }, 1000);
 		});
 	}
 
-	function addAlbumsUser() {
+	function addUsersAlbums() {
+		var arrAlbum = [];
 		$(document).on('click', '.btn-albums', function(e) {
 			e.preventDefault();
 			$this = $(this);
 			let $user = $this.parent();
 			let id = $user.data('id');
-			let availability = false;
 
-			$('.content').append('<div class="loader"></div>');
-			$('.btn').removeClass('active');
-			$('.user').removeClass('active');
+			// $('.content').append('<div class="loader"></div>');
+			removeContent();
 			$this.addClass('active');
 			$user.addClass('active');
-			$('.post').remove();
-			$('.album').remove();
-			$('.album-foto').remove();
-			$('.todo').remove();
+			
 
 			if (typeof arrAlbum['userId_' + id] === 'undefined') {
+				loaderShow();
 				$.ajax({
 					url: `https://jsonplaceholder.typicode.com/albums?userId=${id}`,  
 					dataType : "json",
 					async: false,
+					// beforeSend: function() {
+					// 	loaderShow();
+					// },
 					success: function (data) { 
 						arrAlbum['userId_' + id] = data;
+					},
+					error: function() {
+						alert('Error');
+					},
+					complete: function() {
+						loaderHide();
 					}
 				})
 			} 
 
 			for (let index = 0; index < arrAlbum[`userId_${id}`].length; index++) {
-				if (arrAlbum[`userId_${id}`][index].userId === id) {
-					$('.posts .row').append(`
-						<div class="col-3 album" data-albums="${arrAlbum[`userId_${id}`][index].id}">
-							<div class="title">${arrAlbum[`userId_${id}`][index].title}</div>
-						</div>
-					`)
-				}
-			}
-			setTimeout(function () {
-				$('.loader').remove()
-			}, 1000);
+				$('.posts .row').append(`
+					<div class="col-3 album" data-albums="${arrAlbum[`userId_${id}`][index].id}">
+						<div class="title">${arrAlbum[`userId_${id}`][index].title}</div>
+					</div>
+				`)
+			};
+
+			// setTimeout(function() {
+			// 	// $('.loader').remove();
+			// }, 1000);
 		});
 	}
 
 
-	function addAlbumUser() {
+	function addFotoAlbum() {
+		var arrFoto = [];
 		$(document).on('click', '.album', function() {
-			$this = $(this);
-			
-			let id = $this.data('albums');
-			let availability = false;
 
-			$('.content').append('<div class="loader"></div>');
+			let id = $(this).data('albums');
+
+			//$('.content').append('<div class="loader"></div>');
 			$('.album').remove();
-			$('.album-foto').remove();
 			
-			for (let index = 0; index < arrFoto.length; index++) {
-				if (arrFoto[index].albumId === id) {
-					availability = true;
-				}
-			}
-			
-			if (!availability) {
+			if (typeof arrFoto['albumId_' + id] === 'undefined') {
+				loaderShow();
 				$.ajax({
 					url: `https://jsonplaceholder.typicode.com/photos?albumId=${id}`, 
 					dataType : "json",
 					async: false,
+					// beforeSend: function() {
+					// 	loaderShow();
+					// },
 					success: function (data) { 
-						for (let index = 0; index < data.length; index++) {
-							arrFoto.push(data[index]);
-						}
+						arrFoto['albumId_' + id] = data;
+					},
+					error: function() {
+						alert('Error');
+					},
+					complete: function() {
+						loaderHide();
 					}
 				})
 			}
 
-			for (let index = 0; index < arrFoto.length; index++) {
-				if (arrFoto[index].albumId === id) {
-					$('.posts .row').append(`
-						<div class="col-3 album-foto" data-post="${arrFoto[index].id}">
-							<div><img src="${arrFoto[index].thumbnailUrl}"</div>
-							<div class="title">${arrFoto[index].title}</div>
-						</div>
-					`)
-				}
+			for (let index = 0; index < arrFoto['albumId_' + id].length; index++) {
+				$('.posts .row').append(`
+					<div class="col-3 album-foto" data-post="${arrFoto['albumId_' + id][index].id}">
+						<div><img src="${arrFoto['albumId_' + id][index].thumbnailUrl}"</div>
+						<div class="title">${arrFoto['albumId_' + id][index].title}</div>
+					</div>
+				`)
 			}
-			setTimeout(function () {
-				$('.loader').remove()
-			}, 1000);
+			// setTimeout(function () {
+			// 	// $('.loader').remove()
+			// }, 1000);
 		});
 	}
 
-	function addTodosUser() {
+	function addUsersTodos() {
+		arrTodo = [];
 		$(document).on('click', '.btn-todos', function(e) {
 			e.preventDefault();
 			$this = $(this);
 			let $user = $this.parent();
 			let id = $user.data('id');
-			let availability = false;
 
-			$('.content').append('<div class="loader"></div>');
-			$('.btn').removeClass('active');
-			$('.user').removeClass('active');
+			// $('.content').append('<div class="loader"></div>');
+			removeContent ();
 			$this.addClass('active');
 			$user.addClass('active');
-			$('.post').remove();
-			$('.album').remove();
-			$('.album-foto').remove();
-			$('.todo').remove();
-
-			for (let index = 0; index < arrTodo.length; index++) {
-				if (arrTodo[index].userId === id) {
-					availability = true;
-				}
-			}
 			
-			if (!availability) {
+			if (typeof arrTodo['userId_' + id] === 'undefined') {
+				loaderShow();
 				$.ajax({
 					url: `https://jsonplaceholder.typicode.com/todos?userId=${id}`, 
 					dataType : "json",
 					async: false,
+					// beforeSend: function() {
+					// 	loaderShow();
+					// },
 					success: function (data) { 
-						for (let index = 0; index < data.length; index++) {
-							arrTodo.push(data[index]);
-						}
+						arrTodo['userId_' + id] = data;
+					},
+					error: function() {
+						alert('Error');
+					},
+					complete: function() {
+						loaderHide();
 					}
 				})
 			}
 
-			for (let index = 0; index < arrTodo.length; index++) {
-				if (arrTodo[index].userId === id) {
-					// let check;
-					// if (arrTodo[index].completed === true) {
-					// 	check = checked;
-					// }
-					$('.posts .row').append(`
-						<div class="col-3 todo" data-todo="${arrTodo[index].id}">
-							<div class="todo-title">${arrTodo[index].title}</div>
-							<input type="checkbox">
-						</div>
-					`)
-					
+			for (let index = 0; index < arrTodo['userId_' + id].length; index++) {
+				let check;
+				if ( arrTodo['userId_' + id][index].completed === true) {
+					check = '<input type="checkbox" checked>';
+				} else {
+					check = '<input type="checkbox">';
 				}
+				$('.posts .row').append(`
+					<div class="col-3 todo" data-todo="${arrTodo['userId_' + id][index].id}">
+						<div class="todo-title">${arrTodo['userId_' + id][index].title}</div>
+						${check}
+					</div>`
+				);
 			}
-			setTimeout(function () {
-				$('.loader').remove()
-			}, 1000);
+			// setTimeout(function () {
+			// 	// $('.loader').remove()
+			// }, 1000);
 		});
 	}
 
+	function removeContent () {
+		$('.btn').removeClass('active');
+		$('.user').removeClass('active');
+		$('.post').remove();
+		$('.album').remove();
+		$('.album-foto').remove();
+		$('.todo').remove();
+	} 
 
+	function loaderShow() {
+		$('.loader').show(1000)
+	}
+	function loaderHide() {
+		$('.loader').hide(1000)
+	}
 
-	addUsers ();
-	addPostUser();
-	addComment ();
-	addAlbumsUser();
-	addAlbumUser();
-	addTodosUser()
-
-})
+	$(document).ready(function() {
+		addUsersPost();
+		addUsers();
+		addComment();
+		addUsersAlbums();
+		addFotoAlbum();
+		addUsersTodos();
+		// loaderHide();
+	})
 })(jQuery);

c6007b58dbbc33f644075f6dd60605a3e6a0edf6
Oleh-99 <cyaputa99@gmail.com>
2021-04-22 17:56:46 +0300

    add edit

--- lesson-8/js/main.js
+++ lesson-8/js/main.js
@@ -1,34 +1,43 @@
 (function($) {
 	var apiKey = 'WVEgiSInGXai1zSVMYF8CWe5huRwfA-cS4W7RmUDQEg';
+	var cacheSearch = [];
+	var filter = ['item1', 'item2', 'item3', 'item4', 'item2', 'item1', 'item2', 'item3', 'item4', 'item1', 'item3', 'item4',];
 
 	function loadingPage() {
 		if (!sessionStorage.getItem('dataPhoto')) {
 			$.ajax({
 				url: `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=12`, 
-				
 				success: function(data) {
 					sessionStorage.setItem('dataPhoto', JSON.stringify(data));
+					renderCard();
 					creatingSlider();
 				},
 				error: function() {
 					alert('Error');
-				}
+				},
 			});
 		} else {
 			creatingSlider();
+			renderCard();
 		}
 	}
 
 	function creatingSlider() {
 		let data = JSON.parse(sessionStorage.getItem('dataPhoto'));
+		var $sliderWrapper = $('.single-item');
 
 		for (let index = 0; index < data.length; index++) {
-			$('.single-item').append(
+			$sliderWrapper.append(
 				`<div>
-					<img src="${data[index].urls.raw}&fit=crop&w=350&h=350" alt="">
+					<img src="${data[index].urls.raw}&fit=crop&w=350&h=350" alt="">						
 				</div>`
 			);
-		}
+		};
+
+		$sliderWrapper.imagesLoaded()
+			.done(function(instance) {
+				initSlider();
+			});
 	}
 
 	function initSlider() {
@@ -83,33 +92,35 @@
 	}
 
 	function search() {
-		var info = {};
-
-		if (sessionStorage['Search']) {
-			info = JSON.parse(sessionStorage.getItem('Search'));
-		}
-
 		$('.input-search').on('keyup', function() {
 			var $thisInput = $('.input-search').val();
 			
-			if(2 <= $thisInput.length) {
-				if('undefined' === typeof info[$thisInput]) {
+			if (2 <= $thisInput.length) {
+				if ('undefined' === typeof cacheSearch[$thisInput]) {
+					removeSearch();
+					addLoaderSearch();
+
 					$.ajax({
 						url: `https://api.unsplash.com/search?query=${$thisInput}&client_id=${apiKey}`, 
-						
 						success: function(data) {
-							info[$thisInput] = data;
-							sessionStorage.setItem('Search', JSON.stringify(info));
-							removeSearch();
-							renderSearch(info[$thisInput]);
+							if (0 !== data.photos.total) {
+								cacheSearch[$thisInput] = data;
+								renderSearch(cacheSearch[$thisInput]);
+							} else {
+								removeLoaderSearch();
+								$('.render-search').append(
+									`<div class="info">Not information</div>`
+								);
+							}
 						},
 						error: function() {
 							alert('Error');
-						}
+						},
 					});
 				} else {
 					removeSearch();
-					renderSearch(info[$thisInput]);
+					addLoaderSearch();
+					renderSearch(cacheSearch[$thisInput]);
 				}		
 			} else {
 				removeSearch();
@@ -117,19 +128,93 @@
 		});
 	}
 
-	function renderSearch(date) {
-		$('.input-wrapper').append(`<div class="render-search"></div>`);
-
-		for (let index = 0; index < date.photos.results.length; index++) {
-
+	function renderSearch(data) {
+		for (let index = 0; index < data.photos.results.length; index++) {
 			$('.render-search').append(
-				`<div class="result"><img src="${date.photos.results[index].urls.thumb}" alt=""></div>`
+				`<div class="result" data-value="${index}"><img src="${data.photos.results[index].urls.raw}&fit=crop&w=300&h=300" alt=""></div>`
 			);
+
+			$('.result').imagesLoaded()
+			.done(function(instance) {
+				$('.result').slideDown(300);
+				removeLoaderSearch();
+			});	
 		}
 	}
 
 	function removeSearch() {
-		$('.render-search').remove();
+		$('.render-search').slideUp(3000).remove();
+	}
+
+	function addLoaderSearch() {
+		$('.input-wrapper').append(`<div class="render-search"></div>`);
+		$('.render-search').slideDown(3000).append(`<div class="loader-result"><div class="loader-gif"></div></div>`);
+	}
+
+	function removeLoaderSearch() {
+		$('.loader-result').remove();
+	}
+
+	function viewSearchFoto() {
+		$(document).on('click', '.result', function() {
+			var $this = $(this); 
+			var id = $this.data('value');
+			var searchKey = $this.parent().prev().val();
+			var usersInfo = cacheSearch[searchKey].photos.results[id];
+			
+			removeSearch();
+			$('.slider-wrapper').remove();
+			$('.filters').remove();
+			$('.our-user').remove();
+
+			$.ajax({
+				url: `${usersInfo.user.links.photos}?client_id=${apiKey}`, 
+				success: function(data) {
+					fotoUsers(data);
+				},
+				error: function() {
+					alert('Error');
+				},
+			});
+
+			$('body').append(
+				`<section class="our-user">
+					<div class="container">
+						<div class="profile">
+							<div class="profile-foto">
+								<img src="${usersInfo.user.profile_image.large}">
+							</div>
+							<div class="profile-name">
+								<h4>${usersInfo.user.name}</h4>
+								<p></p>
+							</div>
+						</div>
+					</div>
+				</section>`
+			);
+
+			function fotoUsers(data) {
+				$('.our-user').find('.container').append('<div class="row"></div>');
+
+				let $profileWrapper = $('.our-user').find('.row');
+
+				for (let index = 0; index < data.length; index++) {
+					$profileWrapper.append(
+						`<div class="col-sm-12 col-md-6 col-lg-4 foto-wrapper">
+						<a href="${data[index].urls.regular}" class="foto-view" data-width="${data[index].width}" data-height="${data[index].height}"><img src="${data[index].urls.raw}&fit=crop&w=350&h=350" alt=""></a>
+							<div class="foto-inner">
+								<a href="${data[index].links.download}?force=true" class="button"  data-toggle="tooltip" data-placement="top" title="Download"><i class="far fa-cloud-download-alt"></i></a>
+							</div>
+						</div>`
+					);
+				}
+				$profileWrapper.imagesLoaded()
+					.done(function(instance) {
+						$profileWrapper.slideDown(300);
+						fotoSwipe();
+					});	
+			}
+		});
 	}
 
 	function renderCard() {
@@ -139,10 +224,16 @@
 			if (!data[index].user.bio) {
 				data[index].user.bio = '';
 			}
+
 			$('.cards-wrapper').append(
-				`<div class="col-ms-12 col-md-6 col-lg-4 col-md-12">
+				`<div class="col-ms-12 col-md-6 col-lg-4 col-md-12 filters-inner ${filter[index]}" data-value="${filter[index]}">
 					<div class="card">
-						<img class="card-img-top" src="${data[index].urls.raw}&fit=crop&w=350&h=200" alt="Card image cap">
+						<div class="foto-wrapper">
+							<a href="${data[index].urls.regular}" class="foto-view" data-width="${data[index].width}" data-height="${data[index].height}"><img class="card-img-top" src="${data[index].urls.raw}&fit=crop&w=350&h=200" alt="Card image cap"></a>
+							<div class="foto-inner">
+								<a href="${data[index].links.download}?force=true" class="button" data-toggle="tooltip" data-placement="top" title="Download"><i class="far fa-cloud-download-alt"></i></a>
+							</div>
+						</div>						
 						<div class="card-body">
 							<h5 class="card-title">${data[index].user.name}</h5>
 							<p class="card-text">${data[index].user.bio}</p>
@@ -155,14 +246,108 @@
 			);
 		}
 	}
-		
 
+	function fotoSwipe() {
+		if ('undefined' === typeof PhotoSwipe) {
+			return;
+		}
+
+		var $pswp = $('.pswp')[0];
+		var attr = $('.filter').find('.active').data('filter');
+		console.log(attr);
+
+		if('*' !== attr) {
+			var filter = attr;
+		} else {
+			var filter = '.filters-inner';
+		}
+
+		$('.cards-wrapper').each( function() {
+			var $pic = $(this).find(filter),
+				getItems = function() {
+					var items = [];
+					$pic.each(function() {
+						
+						var $this = $(this).find('.foto-view');
+						var item = {
+							src: $this.attr('href'),
+							w: $this.data('width'),
+							h: $this.data('height'),
+						};
+	
+						items.push(item);
+					});
+					return items;
+				}
+				
+			var items = getItems();
+	
+			$pic.on('click', '.foto-view', function(event) {
+				event.preventDefault();
+				
+				var $index = $(this).parents('.filters-inner').index();
+				var options = {
+					index: $index,
+					bgOpacity: 0.7,
+					showHideOpacity: true
+				};
+	
+				new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options).init();
+			});
+		});
+	}
+
+	function filterProduct() {
+		var $shopGrid = $('.cards-wrapper');
+
+		if (0 === $shopGrid.length || 'undefined' === typeof Isotope) {
+			return;
+		}
+
+		$shopGrid.imagesLoaded()
+			.done(function(instance) {
+				filterloaded();
+			});
+
+		function filterloaded() {
+			var grid = new Isotope('.cards-wrapper', {
+				itemSelector: '.filters-inner',
+				layoutMode: 'fitRows',
+			});
+	
+			$('.filter').on('click', 'li', function(e) {
+				e.preventDefault();
+				let $this = $(this);
+				let filterData = $this.data('filter');
+	
+				$this.siblings().removeClass('active');
+				$this.addClass('active');
+	
+				grid.arrange({
+					filter: filterData,
+				});
+				fotoSwipe();
+			});
+		}
+	}
+
+	function tooltip() {
+		var $btnTooltip = $('[data-toggle="tooltip"]');
+
+		if ('undefined' === typeof tooltip || 0 === $btnTooltip.length) {
+			return;
+		}
+
+		$btnTooltip.tooltip();
+	}
 
 	$(document).ready(function() {
 		loadingPage();
-		initSlider();
 		search();
-		renderCard();
+		viewSearchFoto();
+		fotoSwipe();
+		filterProduct();
+		tooltip();
 	});
 
 })(jQuery);
